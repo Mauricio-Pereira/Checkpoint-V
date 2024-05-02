46101d9eb67f4b7fb006742534d3a6eb5cf4b758
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-05-02 08:29:19 -0300

    ajustes de layout

--- checkpoint-v/src/app/layout.tsx
+++ checkpoint-v/src/app/layout.tsx
@@ -1,3 +1,6 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   );
 }
