26562b718da517022195c78ba1944a58ec1aec18
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-05-02 10:58:43 -0300

    modificações

--- checkpoint-v/src/components/Footer/index.tsx
+++ checkpoint-v/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <footer className="flex">
-                <img className='imagem-footer' src="/logoSalesForce.png" alt="logoSalesForce.png" title="© Copyright 2023 Salesforce"/>
+                <img className='imagem-footer' src="/logoSalesForce.png" alt="logoSalesForce.png" title="© Copyright 2023 Sneakers S.A"/>
                 <p className="copy">&copy; Copyright 2024 Checkpoint S.A</p>
             </footer>
         </>
