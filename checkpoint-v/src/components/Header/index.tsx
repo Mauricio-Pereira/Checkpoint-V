d46cdd78afc6ef1231bc3c7b8e7d97ab4e821374
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-05-02 08:22:07 -0300

    criação do header e footer

---
+++ checkpoint-v/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <>
+            <header className="flex">
+                <nav>
+                    <ul>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Header;
