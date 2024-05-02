6ec4ebc89d46b992b9aea0233ea9c4e7c4b14205
Mauricio Pereira <mauricio.pvieira1@gmail.com>
2024-05-02 11:27:12 -0300

    ajustando header

--- checkpoint-v/src/components/Header/index.tsx
+++ checkpoint-v/src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import './styles.css';
+import Image from 'next/image';
 
 const Header = () => {
     return (
         <>
-            <header className="flex">
-                <nav>
+            <header className="flex header-options">
+                <nav><Link href={"/"}><Image src="/img/logo-laranja.png" width={200} height={200} alt="Logo da empresa" className='img'/></Link>
                     <ul>
                         <li><Link className="links" href="/">Página Inicial</Link></li>
                         <li><Link className="links" href="/Produtos">Produtos</Link></li>
