9a491861c0e84064626c8770dfaf7f2118b6d34e
Mauricio Pereira <mauricio.pvieira1@gmail.com>
2024-05-02 09:14:47 -0300

    criando componente e pagina Produto

--- checkpoint-v/src/app/Produtos/page.tsx
+++ checkpoint-v/src/app/Produtos/page.tsx
@@ -1,7 +1,14 @@
+import Produto from "../../components/Produto"
+
 export default function Produtos() {
     return (
       <main className="flex-auto">
-        Produtos
-      </main>
+      <Produto
+      title="TÊNIS NEW BALANCE 9060"
+      image="/new-balance.jpg"
+      link="https://www.artwalk.com.br/tenis-new-balance-9060-u060p-h-001/p"
+      preco={899.99}
+    />
+    </main>
     );
 }
